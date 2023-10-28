7a9dc54e6e859aa99eda1425dd09c8ea8e823cde
Ubuntu <ubuntu@ip-172-31-7-67.us-west-2.compute.internal>
2023-10-28 19:03:55 +0000

    ckd model arch

--- notebooks/models_package/models.py
+++ notebooks/models_package/models.py
@@ -26,3 +26,56 @@ class TeacherModel(nn.Module):
         x = x.view(-1, self.conv1.out_channels * 15 * 15)
         x = self.fc1(x)
         return x
+
+### Classical Knowledge Distillatino
+class Teacher(nn.Module):
+    def __init__(self):
+        super(Teacher, self).__init__()
+        
+        self.layer1 = nn.Linear(3072, 1200)
+        self.layer2 = nn.Linear(1200, 1200)
+        self.layer3 = nn.Linear(1200, 100)
+        
+        self.dropout_20 = nn.Dropout(0.2)
+        self.dropout_50 = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Check input shape and resize if necessary
+        if x.shape[-3:] != (3, 32, 32):
+            x = x[:, :3, :32, :32]
+        
+        x = x.contiguous().view(x.size(0), -1)  # Flatten the input
+        x = self.dropout_20(x)
+        x = F.relu(self.layer1(x))
+        x = self.dropout_50(x)
+        x = F.relu(self.layer2(x))
+        x = self.dropout_50(x)
+        return self.layer3(x)
+
+class Student(nn.Module):
+    def __init__(self, use_dropout=False):
+        super(Student, self).__init__()
+        self.use_dropout = use_dropout
+        
+        self.layer1 = nn.Linear(3072, 800)
+        self.layer2 = nn.Linear(800, 800)
+        self.layer3 = nn.Linear(800, 100)
+        
+        self.dropout_20 = nn.Dropout(0.2)
+        self.dropout_50 = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Check input shape and resize if necessary
+        if x.shape[-3:] != (3, 32, 32):
+            x = x[:, :3, :32, :32]
+            
+        x = x.contiguous().view(x.size(0), -1)  # Flatten the input
+        if self.use_dropout:
+            x = self.dropout_20(x)
+        x = F.relu(self.layer1(x))
+        if self.use_dropout:
+            x = self.dropout_50(x)
+        x = F.relu(self.layer2(x))
+        if self.use_dropout:
+            x = self.dropout_50(x)
+        return self.layer3(x)
