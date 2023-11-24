6bcdc1167eaab18d4d0e6b8714714818ab91ca57
Ubuntu <ubuntu@ip-172-31-6-94.us-west-2.compute.internal>
2023-11-24 12:32:05 +0000

    rkd with wider

--- notebooks/utils/misc_tools.py
+++ notebooks/utils/misc_tools.py
@@ -188,7 +188,157 @@ def best_LR_wider(model, trainloader, criterion, optimizer, scheduler, device, n
     
     print(f'Best learning rate: {best_lr}')
     return best_lr
+
+
+################################ RKD Functions ################################
+def best_lr_rkd(model, dataloader, criterion, optimizer, scheduler, device, num_epochs=5, lr_range=(1e-4, 1e-1), plot_loss=True):
+    model.train()
+    model.to(device)
+    lr_values = np.logspace(np.log10(lr_range[0]), np.log10(lr_range[1]), num_epochs * len(dataloader))  # Generate learning rates for each batch
+    lr_iter = iter(lr_values)
+    losses = []
+    lrs = []
     
+    for epoch in range(num_epochs):
+        for i, batch in enumerate(tqdm(dataloader)):
+            lr = next(lr_iter)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr  # Set new learning rate
+            
+            inputs, labels = batch['img'].to(device), batch['label'].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            losses.append(loss.item())
+            lrs.append(lr)
+    
+    # Calculate the derivative of the loss
+    loss_derivative = np.gradient(losses)
+    
+    # Find the learning rate corresponding to the minimum derivative (steepest decline)
+    best_lr_index = np.argmin(loss_derivative)
+    best_lr = lrs[best_lr_index]
+    
+    if plot_loss:
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(lrs, losses)
+        plt.xscale('log')
+        plt.xlabel('Learning Rate')
+        plt.ylabel('Loss')
+        plt.title('Learning Rate Range Test')
+        plt.axvline(x=best_lr, color='red', linestyle='--', label=f'Best LR: {best_lr}')
+        plt.legend()
+        plt.show()
+    
+    print(f'Best learning rate: {best_lr}')
+    return best_lr
+
+def rkd_train_teacher(model, dataloader, criterion, optimizer, scheduler, device, num_epochs=5, patience=5):
+    model.train()
+    model.to(device)
+    best_train_loss = float('inf')
+    patience_counter = 0
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        epoch_loss = 0.0  
+        num_batches = 0  
+        for i, batch in enumerate(tqdm(dataloader)):            
+            inputs, labels = batch['img'].to(device), batch['label'].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            epoch_loss += loss.item()
+            num_batches += 1
+            if i % 100 == 99:  # Print every 100 mini-batches
+                print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100:.3f}")
+                running_loss = 0.0
+
+        epoch_loss /= num_batches  
+        
+        # Check for early stopping
+        if epoch_loss < best_train_loss:
+            best_train_loss = epoch_loss
+            patience_counter = 0 
+            # checkpoint
+            torch.save(model.state_dict(), f'teacher_model_weights_rkd_prof_checkpoint.pth')
+            torch.save(model, f'teacher_model_rkd_prof_checkpoint.pth')
+
+        else:
+            patience_counter += 1
+
+        if patience_counter >= patience:
+            print('Early stopping')
+            break
+
+        scheduler.step()
+
+    print("Finished Training Teacher")
+
+# Function to train the student model with knowledge distillation
+def rkd_train_student_with_distillation(student, teacher, dataloader, criterion, optimizer, scheduler, device, alpha, temperature, num_epochs, patience=5):
+    student.train()
+    teacher.eval()
+    student.to(device)
+    teacher.to(device)
+    best_train_loss = float('inf')  
+    patience_counter = 0 
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0 
+        epoch_loss = 0.0  
+        num_batches = 0  
+        for i, batch in enumerate(tqdm(dataloader)):
+            inputs, labels = batch['img'].to(device), batch['label'].to(device)
+            optimizer.zero_grad()
+            student_outputs = student(inputs)
+            with torch.no_grad():
+                teacher_outputs = teacher(inputs)
+            # ce_loss = criterion(student_outputs, labels)
+            # kd_loss = tkd_kdloss(student_outputs, teacher_outputs, temperature=temperature)  # from utils.loss_functions
+            # loss = alpha * kd_loss + (1 - alpha) * ce_loss
+
+            distance_loss = RKDDistanceLoss()(student_outputs, teacher_outputs)
+            angle_loss = RKDAngleLoss()(student_outputs, teacher_outputs)
+            loss = criterion(student_outputs, labels) + 0.1 * (distance_loss + angle_loss)
+
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            epoch_loss += loss.item()
+            num_batches += 1
+            if i % 100 == 99:  
+                print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100:.3f}")
+                running_loss = 0.0
+
+        epoch_loss /= num_batches  
+
+        # Check for early stopping
+        if epoch_loss < best_train_loss:
+            best_train_loss = epoch_loss
+            patience_counter = 0 
+            torch.save(student.state_dict(), f'student_model_weights_rkd_prof_checkpoint.pth')
+            torch.save(student, f'student_model_rkd_prof_checkpoint.pth')
+        else:
+            patience_counter += 1 
+
+        if patience_counter >= patience:
+            print('Early stopping')
+            break  
+
+        scheduler.step() 
+
+    print("Finished Training Student")
+
+################################ RKD Functions Ended ################################
+
 
 #### Norm and Direction code helper functions ##
 
